18a27aa524542a989636aea45cb9464bf84c7a74
Suraj Kumar <sk96.uk@gmail.com>
2026-03-30 13:14:40 +0100

    test

--- packages/cri-logger/src/index.ts
+++ packages/cri-logger/src/index.ts
@@ -1,3 +1,6 @@
 import { Logger } from "@aws-lambda-powertools/logger";
 
 export const logger = new Logger();
+
+export const testLogger = (serviceName: string): Logger =>
+  new Logger({ serviceName });
