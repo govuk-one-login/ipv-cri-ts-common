34318ff2b020b54b2cd930b5c709bbd494632190
Ulric Perinette MabambTene <ulric.tene@digital.cabinet-office.gov.uk>
2026-09-24 12:03:03 +0100

    fix build failure

--- packages/cri-healthcheck-assertion/src/stringify-error.ts
+++ packages/cri-healthcheck-assertion/src/stringify-error.ts
@@ -1,7 +1,7 @@
 export function safeStringifyError(error: unknown): string {
   const errorType = error instanceof Error ? error.name : typeof error;
 
-  if (process.env.LOG_FULL_ERRORS === "true") {
+  if (process.env["LOG_FULL_ERRORS"] === "true") {
     return `${errorType} (${String(error)})`;
   }
 
